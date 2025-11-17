478419cfdee778664a1288f713e51dbed9285eb5
Marc Fraile <marc.fraile.fabrega@gmail.com>
2025-11-17 14:40:13 -0500

    local/server/__init__: only import public classes and functions from sub-modules.

--- local/server/__init__.py
+++ local/server/__init__.py
@@ -9,6 +9,7 @@ Provides server registration utilities `register_server()`, `register_defaults()
 """
 
 
-from .base import *
-from .lookup import *
-from .register import *
+from .base import Population, RefPanel, Server, normalize_name
+from .lookup import get_all_servers, get_server
+from .register import register_server
+from .display import RefPanelDisplay, ServerDisplay, display_refpanel, display_server
