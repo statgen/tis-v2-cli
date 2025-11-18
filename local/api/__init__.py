ace8a3bef638685847838a6a500d706ec1f9267d
Marc Fraile <marc.fraile.fabrega@gmail.com>
2025-11-18 11:02:35 -0500

    local.api: add top-level documentation and explicitly export public items.

--- local/api/__init__.py
+++ local/api/__init__.py
@@ -1,2 +1,9 @@
-from .base import *
-from .helpers import *
+"""
+Provides `TisV2Api`, a class to manage all calls to and from a specific server.
+
+Also provides `get_api(server, **kwargs)` to obtain a `TisV2Api` instance that points to a server by name (or by `Server` class).
+"""
+
+
+from .base import TisV2Api, AdminKillAllResponse
+from .helpers import get_api
